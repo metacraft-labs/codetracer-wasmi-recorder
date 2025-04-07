a640a92e37b836f6ac3a28004140ae5d5909bb88
Petar Atanasov <p.atanasov21@abv.bg>
2025-04-07 16:40:12 +0300

    feat(nix): Define initial nix dev shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          pkgs,
+          inputs',
+          self',
+          ...
+        }:
+        {
+
+          devShells.default = import ./shell.nix { inherit pkgs self' inputs'; };
+
+        };
+    };
+}
